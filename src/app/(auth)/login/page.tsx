923296c63e64204f90436b5fe1e9e8a30d164bb2
Kazz <i94773007@gmail.com>
2024-10-02 00:15:21 +0200

    wrapped LoginForm with a Suspense because Vercel forces me to (hopefully never to be seen, i dont know what to add as a fallback

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,12 +2,15 @@
 import { LoginForm } from '@/components/auth/LoginForm'
 import { Container } from '@/components/Container'
 import Link from 'next/link'
+import { Suspense } from 'react'
 
 export default function LoginPage() {
 	return (
 		<Container asSection className='mt-6 max-w-96 flex flex-col gap-1'>
 			<h1 className="text-2xl mb-2 text-center">Login</h1>
-			<LoginForm />
+			<Suspense fallback={<div>Loading login form...</div>} >
+				<LoginForm />
+			</Suspense>
 			<Link href='/register' className="text-cyan-400 mt-4 text-center w-fit mx-auto">
 				Don&apos;t have an account yet? Register here!
 			</Link>
