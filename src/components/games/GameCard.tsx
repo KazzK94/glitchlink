fc604e122da2bddedd633ffd14dd81848aa4d6d7
Kazz <i94773007@gmail.com>
2024-10-02 14:32:29 +0200

    removed Image elements for now, to reduce costs

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from '@/components/ui/button'
 
@@ -10,23 +9,25 @@ interface GameCardProps {
 }
 
 export function GameCard({ title, imageUrl }: GameCardProps) {
+
+	const croppedImageUrl = imageUrl.replace('media/', 'media/crop/600/400/')
+
 	return (
 		<Card
 			className="relative overflow-hidden group h-[260px] md:h-[320px] transition-all duration-300 ease-in-out transform hover:scale-105"
 		>
-			<Image
-				src={imageUrl.replace('media/', 'media/crop/600/400/')}
+			<img
+				src={croppedImageUrl}
 				alt={title}
-				fill
-				sizes='600px'
-				className="transition-opacity object-cover duration-300 ease-in-out group-hover:opacity-80"
+				className="transition-opacity h-full object-cover duration-300 ease-in-out group-hover:opacity-90"
 			/>
 			<CardContent className="absolute inset-0 flex flex-col justify-end p-4 bg-gradient-to-t from-black to-transparent">
-				<h2 className="text-2xl font-bold text-white text-pretty text-center mb-2">{title}</h2>
+				<h2 className="text-2xl font-bold text-white text-pretty text-center mb-1">{title}</h2>
 				<Button
 					variant='secondary'
-					className={`w-full text-sm overflow-hidden transition-all duration-300 ease-in-out 
-						max-h-0 opacity-0 group-hover:max-h-[200px] group-hover:opacity-100 -mb-5 group-hover:mb-0 group-hover:mt-1`}
+					className={`w-full text-sm overflow-hidden invisible h-0 p-0
+						transition-all duration-300 ease-in-out 
+						group-hover:h-9 group-hover:visible group-hover:p-2 group-hover:mt-1`}
 					onClick={() => alert(`Game "${title}" added (nah, not really, TBI)`)}
 				>
 					Add to my games
