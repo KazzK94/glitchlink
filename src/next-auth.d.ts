8945f35ad8fcabac1d30d114b2b767833bed0833
Kazz <i94773007@gmail.com>
2024-10-02 14:33:20 +0200

    added next-auth types definition file for sessions types

---
+++ src/next-auth.d.ts
@@ -0,0 +1,27 @@
+// types/next-auth.d.ts
+import 'next-auth'
+import 'next-auth/jwt'
+
+declare module 'next-auth' {
+	interface Session {
+		user: User
+	}
+
+	interface User {
+		id: string
+		name: string
+		username: string
+		email: string
+		color: string
+	}
+}
+
+declare module 'next-auth/jwt' {
+	interface JWT {
+		id: string
+		name: string
+		username: string
+		email: string
+		color: string
+	}
+}
