9b9e4f5e865727631c84ae9c3936e5b035c112f1
Kazz <i94773007@gmail.com>
2024-10-10 22:28:07 +0200

    changed texts

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -24,11 +24,11 @@ export function Home() {
 function NewPostForm() {
 	return (
 		<div className="bg-gray-800 px-6 py-5 rounded-lg">
-			<h2 className="text-xl font-semibold mb-4">What&apos;s on your mind, Gamer?</h2>
+			<h2 className="text-xl font-semibold mb-4">What&apos;s on your mind?</h2>
 			<textarea
 				className="w-full bg-gray-700 text-white rounded-md p-3 mb-3"
 				rows={3}
-				placeholder="Share your gaming thoughts..."
+				placeholder="Share your thoughts..."
 			></textarea>
 			<div className='flex w-full justify-end'>
 				<Button className="w-full text-base bg-gradient-to-r from-cyan-500 to-purple-500 text-white px-4 py-2 rounded-md hover:from-cyan-600 hover:to-purple-600">
