4f03d5d46927b0e743953d1c19404dde2c4b1e2e
Kazz <i94773007@gmail.com>
2024-10-10 22:28:50 +0200

    changed from forced cache to revalidation every 3 days

--- src/services/gamesExternalApi.ts
+++ src/services/gamesExternalApi.ts
@@ -1,7 +1,7 @@
 
 export async function getGamesFromExternalApi({ search = '', page = 1, pageSize = 12 }: { search?: string, page?: number, pageSize?: number } = {}) {
 	const response = await fetch(`https://api.rawg.io/api/games?key=8941ce3c4a6248cebc73ad45ffc780c2&page=${page}&page_size=${pageSize}&search=${search}`, {
-		cache: 'force-cache'
+		next: { revalidate: 3 * 24 * 60 * 60 /* revalidate every 3 days (revalidate in seconds) */ }
 	})
 	const data = await response.json()
 	return data.results
@@ -9,7 +9,7 @@ export async function getGamesFromExternalApi({ search = '', page = 1, pageSize 
 
 export async function getGameByIdFromExternalApi(id: number) {
 	const response = await fetch(`https://api.rawg.io/api/games/${id}?key=8941ce3c4a6248cebc73ad45ffc780c2`, {
-		cache: 'force-cache'
+		next: { revalidate: 3 * 24 * 60 * 60 /* revalidate every 3 days (revalidate in seconds) */ }
 	})
 	const data = await response.json()
 	return data
