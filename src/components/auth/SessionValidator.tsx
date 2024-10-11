6675b3c1713032aa1118333f744686558933c6aa
Kazz <i94773007@gmail.com>
2024-10-11 12:56:52 +0200

    completed session validator

--- src/components/auth/SessionValidator.tsx
+++ src/components/auth/SessionValidator.tsx
@@ -4,14 +4,18 @@ import { Session } from 'next-auth'
 import { signOut } from 'next-auth/react'
 import { useEffect } from 'react'
 
-export function SessionValidator({ session }: { session: Session }) {
-
+export function SessionValidator({ session }: { session: Session | null }) {
 	useEffect(() => {
 		async function validateSession() {
-			if (!session?.user) {
-				await signOut()
+			if (!session) return
+			if (!session.user) {
+				return await signOut()
 			}
-			// TODO: Check if user still exists in the DB
+			// Check if user still exists in the DB
+			const res = await fetch('/api/users/me')
+			if(!res.ok) {
+				return await signOut()
+			}
 		}
 		validateSession()
 	}, [session])
