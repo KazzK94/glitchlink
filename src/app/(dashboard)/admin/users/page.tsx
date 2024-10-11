dc59e2422315fc9e582ee705799bef22de169aef
Kazz <i94773007@gmail.com>
2024-10-11 13:17:18 +0200

    added color of user to dashboard info

--- src/app/(dashboard)/admin/users/page.tsx
+++ src/app/(dashboard)/admin/users/page.tsx
@@ -15,6 +15,7 @@ export default async function UsersDashboardPage() {
 							<th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
 							<th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Username</th>
 							<th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+							<th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Color</th>
 						</tr>
 					</thead>
 					<tbody className="bg-white divide-y divide-gray-200 text-black">
@@ -26,6 +27,10 @@ export default async function UsersDashboardPage() {
 								<td className="px-4 py-4 whitespace-nowrap">{user.name}</td>
 								<td className="px-4 py-4 whitespace-nowrap text-sm italic">@{user.username}</td>
 								<td className="px-4 py-4 whitespace-nowrap">{user.email}</td>
+								<td className="px-4 py-4 whitespace-nowrap flex items-center gap-1">
+									<span className='inline-block rounded size-3 mt-0.5' style={{ backgroundColor: user.color }} />
+									{user.color}
+								</td>
 							</tr>
 						))}
 					</tbody>
