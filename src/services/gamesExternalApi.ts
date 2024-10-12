dc1be99f78e5de2b9f2c77be07ffdc1e268efb8a
Kazz <i94773007@gmail.com>
2024-10-12 15:51:07 +0200

    extracted base url and base query params from queries to external games api

--- src/services/gamesExternalApi.ts
+++ src/services/gamesExternalApi.ts
@@ -1,6 +1,9 @@
 
+const BASE_URL = 'https://api.rawg.io/api/games'
+const BASE_QUERY_PARAMS = '?key=8941ce3c4a6248cebc73ad45ffc780c2&ordering=-metacritic'
+
 export async function getGamesFromExternalApi({ search = '', page = 1, pageSize = 12 }: { search?: string, page?: number, pageSize?: number } = {}) {
-	const response = await fetch(`https://api.rawg.io/api/games?key=8941ce3c4a6248cebc73ad45ffc780c2&page=${page}&page_size=${pageSize}&search=${search}`, {
+	const response = await fetch(`${BASE_URL}${BASE_QUERY_PARAMS}&page=${page}&page_size=${pageSize}&search=${search}`, {
 		next: { revalidate: 3 * 24 * 60 * 60 /* revalidate every 3 days (revalidate in seconds) */ }
 	})
 	const data = await response.json()
@@ -8,7 +11,7 @@ export async function getGamesFromExternalApi({ search = '', page = 1, pageSize 
 }
 
 export async function getGameByIdFromExternalApi(id: number) {
-	const response = await fetch(`https://api.rawg.io/api/games/${id}?key=8941ce3c4a6248cebc73ad45ffc780c2`, {
+	const response = await fetch(`${BASE_URL}/${id}${BASE_QUERY_PARAMS}`, {
 		next: { revalidate: 3 * 24 * 60 * 60 /* revalidate every 3 days (revalidate in seconds) */ }
 	})
 	const data = await response.json()
