cde60b9df8d348cb73d1efd182efab0985f4e3f5
Kazz <i94773007@gmail.com>
2024-10-15 13:13:03 +0200

    commenting code to debug error in production that doesnt happen in development

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+/*
 import { MessageSquareIcon, Share2Icon, ThumbsUpIcon, EllipsisVerticalIcon } from 'lucide-react'
 import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { PostComment } from './comments/Comment'
 import { PostCommentCreateForm } from './comments/CommentCreateForm'
+ */
 import { CompletePost } from '@/types'
 
 interface PostProps {
@@ -15,14 +17,9 @@ interface PostProps {
 
 export function Post({ post, loggedUserId }: PostProps) {
 
-	// TODO: 
-	// Logged User ID will be used:
-	// 		1) to check if the user is the author (can update/delete the post) or not (can report the post)
-	//   	2) to see if the user has already liked the post
-
 	const userIsAuthor = post.authorId === loggedUserId
 
-	const [isLiked, setIsLiked] = useState(post.likedBy.some(like => like.id === loggedUserId))
+	/* const [isLikedByUser, setIsLikedByUser] = useState(post.likedBy.some(like => like.id === loggedUserId))
 	const [likes, setLikes] = useState(post.likedBy.length)
 
 	const [showComments, setShowComments] = useState(false)
@@ -31,16 +28,16 @@ export function Post({ post, loggedUserId }: PostProps) {
 	}
 
 	const toggleLike = async () => {
-		const previousIsLiked = isLiked
-		setIsLiked(!previousIsLiked)
+		const previousIsLiked = isLikedByUser
+		setIsLikedByUser(!previousIsLiked)
 		const response = await fetch(`/api/posts/${post.id}/likes`, { method: 'PATCH' })
 		if (!response.ok) {
-			setIsLiked(!previousIsLiked)
+			setIsLikedByUser(!previousIsLiked)
 			console.error('Failed to toggle like:', response)
 			return
 		}
 		setLikes(likes + (previousIsLiked ? -1 : 1))
-	}
+	} */
 
 	return (
 		<article key={post.id} className={`bg-gray-800 pt-4 pb-2 rounded-lg ${userIsAuthor && 'border border-blue-800/30'}`}>
@@ -53,27 +50,27 @@ export function Post({ post, loggedUserId }: PostProps) {
 						<p className="text-sm text-gray-400 italic">@{post.author.username}</p>
 					</div>
 				</div>
-				<button className='flex justify-end items-center pb-2 min-w-8'>
+				{/* <button className='flex justify-end items-center pb-2 min-w-8'>
 					<EllipsisVerticalIcon size={24} />
-				</button>
+				</button> */}
 			</div>
 			{/* Post Content */}
 			<p className="mb-5 px-4">{post.content}</p>
 
 			{/* Buttons */}
-			<div className='px-4 sm:px-6 flex justify-evenly sm:justify-between items-center'>
+			{/* <div className='px-4 sm:px-6 flex justify-evenly sm:justify-between items-center'>
 				<div className="flex gap-x-4 flex-grow justify-evenly">
-					<ToggleLikeButton onClick={toggleLike} likesCount={likes} isLiked={isLiked} />
+					<ToggleLikeButton onClick={toggleLike} likesCount={likes} isLikedByUser={isLikedByUser} />
 					<ToggleCommentsButton onClick={toggleComments} commentsCount={post.comments.length} />
 					<ShareButton postId={post.id} />
 				</div>
 				<div className='hidden sm:block'>
 					<p className="text-sm text-muted opacity-60 italic">{post.createdAt.toLocaleString()}</p>
 				</div>
-			</div>
+			</div> */}
 
 			{/* Comments (conditionally rendered) */}
-			{showComments && (
+			{/* {showComments && (
 				<div className='mx-1 px-1 mt-2 pt-2 border-t border-gray-600'>
 					<PostCommentCreateForm post={post} className='mb-3' />
 					<div className='flex flex-col gap-2'>
@@ -84,14 +81,14 @@ export function Post({ post, loggedUserId }: PostProps) {
 						}
 					</div>
 				</div>
-			)}
+			)} */}
 		</article>
 	)
 }
-
-function ToggleLikeButton({ onClick, likesCount, isLiked }: { onClick: () => void, likesCount: number, isLiked: boolean }) {
+/* 
+function ToggleLikeButton({ onClick, likesCount, isLikedByUser }: { onClick: () => void, likesCount: number, isLikedByUser: boolean }) {
 	return (
-		<Button variant="ghost" onClick={onClick} className={`flex items-center gap-1.5 ${isLiked && 'text-blue-500 hover:text-blue-700'}`}>
+		<Button variant="ghost" onClick={onClick} className={`flex items-center gap-1.5 ${isLikedByUser && 'text-blue-500 hover:text-blue-700'}`}>
 			<ThumbsUpIcon size={20} />
 			{(likesCount > 0) && <span>{likesCount}</span>}
 		</Button>
@@ -120,4 +117,4 @@ function ShareButton({ postId }: { postId: string }) {
 			<Share2Icon size={20} />
 		</Button>
 	)
-}
+} */
