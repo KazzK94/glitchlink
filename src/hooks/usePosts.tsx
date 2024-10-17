4866c081ef2fb96c6a6a16921463f1ded360f71d
Kazz <i94773007@gmail.com>
2024-10-18 00:34:13 +0200

    improved posts parsing to detect users before punctuation characters

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,19 +1,27 @@
 'use client'
 
 import Link from 'next/link'
+import { Fragment } from 'react'
 
 export function usePosts() {
 	const parsePostContent = (text: string) => {
 		const words = text.split(/(\s+)/)
 		return words.map((word, index) => {
-			if (/^@\w+$/.test(word)) {
-				return <Link href={`/u/${word.substring(1, word.length)}`} key={index} className="text-blue-500 hover:underline">{word}</Link>
-			} else if (/^#\w+$/.test(word)) {
-				return <Link href='#' key={index} className="text-gray-400 hover:underline">{word}</Link>
-			}
-			return word
+		  // Match mentions: start with @, followed by letter, then letters/numbers/underscores, followed by any characters
+		  const mentionMatch = word.match(/^@([a-zA-Z]\w+)(.*)$/)
+		  if (mentionMatch) {
+			return (
+			  <Fragment key={index}>
+				<Link href={`/u/${mentionMatch[1]}`} className="text-blue-500 hover:underline">@{mentionMatch[1]}</Link>
+				{mentionMatch[2]}
+			  </Fragment>
+			)
+		  } else if (/^#\w+$/.test(word)) {
+			return <Link href='#' key={index} className="text-gray-400 hover:underline">{word}</Link>
+		  }
+		  return word
 		})
-	}
+	  }
 
 	return { parsePostContent }
 }
