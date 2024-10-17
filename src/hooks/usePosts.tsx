94707b59c17f1c8205b3136218c65e7a0fa6ff0d
Kazz <i94773007@gmail.com>
2024-10-17 23:58:26 +0200

    made mentions and hashtags <Link>s

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,13 +1,15 @@
 'use client'
 
+import Link from 'next/link'
+
 export function usePosts() {
 	const parsePostContent = (text: string) => {
 		const words = text.split(/(\s+)/)
 		return words.map((word, index) => {
 			if (/^@\w+$/.test(word)) {
-				return <span key={index} className="text-blue-500">{word}</span>
+				return <Link href={`/u/${word.substring(1, word.length)}`} key={index} className="text-blue-500 hover:underline">{word}</Link>
 			} else if (/^#\w+$/.test(word)) {
-				return <span key={index} className="text-gray-400">{word}</span>
+				return <Link href='#' key={index} className="text-gray-400 hover:underline">{word}</Link>
 			}
 			return word
 		})
