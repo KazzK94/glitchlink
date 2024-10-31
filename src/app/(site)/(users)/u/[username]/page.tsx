6222f92ed5f97419853798952156611e39d93548
Kazz <i94773007@gmail.com>
2024-10-31 15:24:53 +0100

    created UserProfile component

--- src/app/(site)/(users)/u/[username]/page.tsx
+++ src/app/(site)/(users)/u/[username]/page.tsx
@@ -1,5 +1,6 @@
 
 import { Container } from '@/components/common/Container'
+import { UserProfile } from '@/components/users/UserProfile'
 import { getUserByUsername } from '@/services/users'
 
 export default async function UserDetailPage({ params }: { params: { username: string } }) {
@@ -10,20 +11,6 @@ export default async function UserDetailPage({ params }: { params: { username: s
 	if (!user) return <Container asSection className='mt-8 text-xl text-center italic opacity-80'>404: User not found</Container>
 
 	return (
-		<Container className='mt-4'>
-			<h1 className='text-3xl font-semibold'>{user.name}</h1>
-			<p className='italic opacity-80 text-base mt-0.5 ml-1'>@{user.username}</p>
-
-			<h2 className='text-2xl mt-6'>{user.name}&apos;s Video Games</h2>
-			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 mt-3'>
-				{
-					user.videoGames.map((videogame, index) => (
-						<div key={index} className='p-2 bg-slate-600/30 rounded'>
-							<p className='text-center'>{videogame.title}</p>
-						</div>
-					))
-				}
-			</div>
-		</Container>
+		<UserProfile user={user} />
 	)
 }
