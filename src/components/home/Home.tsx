241613a4b3ecb0c24e2d55056b903bc4d364797c
Kazz <i94773007@gmail.com>
2024-10-31 14:50:54 +0100

    improved UI in HomePage (logged)

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -42,16 +42,21 @@ export async function Home() {
 
 
 async function TrendingGames() {
-	const popularGames = await getPopularVideoGames(5)
+	const popularGames = await getPopularVideoGames(3)
 
 	return (
 		<div className="bg-gray-800 p-6 pt-5 rounded-lg">
-			<h2 className="text-xl font-semibold mb-4">Trending Games</h2>
-			<ul className="space-y-2">
+			<h2 className="text-2xl font-semibold mb-4">Trending Games</h2>
+			<ul className="space-y-3">
 				{popularGames.map((game) => (
-					<li key={game.id} className="flex items-center">
-						<Gamepad2Icon className="h-5 w-5 mr-2" />
-						<span>{game.title}</span>
+					<li key={game.id} className="relative h-20 rounded border overflow-hidden cursor-pointer hover:saturate-150 transition-all duration-300">
+						<img className='w-full h-full object-cover' src={game.image} alt={game.title} />
+						<div className='absolute inset-0 flex justify-center items-end pb-2 bg-gradient-to-t from-black to-transparent'>
+							<p className='font-bold text-xl !select-none cursor-pointer'>
+								<Gamepad2Icon className="h-5 w-5 mr-2 inline-block" />
+								{game.title}
+							</p>
+						</div>
 					</li>
 				))}
 			</ul>
@@ -65,7 +70,7 @@ async function SuggestedFriends() {
 
 	return (
 		<div className="bg-gray-800 p-4 pb-5 rounded-lg">
-			<h2 className="text-xl font-semibold pl-2 mb-3">Suggested Friends</h2>
+			<h2 className="text-2xl font-semibold pl-2 mb-3">Suggested Friends</h2>
 			<UsersList users={suggestedFriends} className="space-y-2" cardClassName='hover:bg-gray-700/15' />
 		</div>
 	)
