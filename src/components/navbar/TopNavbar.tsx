6a5f3922f67baa9527cedd6f4c27dc774efdff3f
Kazz <i94773007@gmail.com>
2024-11-03 01:21:00 +0100

    fixed navbar in mobile

--- src/components/navbar/TopNavbar.tsx
+++ src/components/navbar/TopNavbar.tsx
@@ -5,9 +5,9 @@ import NavbarLinksResponsive from './NavbarLinksResponsive'
 
 export function TopNavbar() {
 	return (
-		<header className="bg-gray-900 shadow-md shadow-black/20 z-50">
+		<header className="h-16 bg-gray-900 shadow-md shadow-black/20 z-50">
 			<Container className='py-3 lg:px-2'>
-				<div className="flex justify-between items-center px-1 sm:px-2">
+				<div className="flex justify-between items-center">
 					<NavbarLinksResponsive />
 					<UserMenu />
 				</div>
