5ce10aa83fcb2899201e1a43033aad415bf55827
Kazz <i94773007@gmail.com>
2025-01-25 13:27:45 +0100

    fix: enhance UserDetailPage to include logged-in user context for profile display

--- src/app/(site)/(users)/u/[username]/page.tsx
+++ src/app/(site)/(users)/u/[username]/page.tsx
@@ -1,16 +1,20 @@
 
 import { Container } from '@/components/common/Container'
 import { UserProfile } from '@/components/users/UserProfile'
+import { getUserFromSession } from '@/services/auth'
 import { getUserByUsername } from '@/services/users'
 
 export default async function UserDetailPage({ params }: { params: { username: string } }) {
 
-	const user = await getUserByUsername({ username: params.username })
+	const [loggedUser, user] = await Promise.all([
+		getUserFromSession(),
+		getUserByUsername({ username: params.username })
+	])
 
 	// If no user: show error message "User Not Found"
 	if (!user) return <Container asSection className='mt-8 text-xl text-center italic opacity-80'>404: User not found</Container>
 
 	return (
-		<UserProfile user={user} />
+		<UserProfile user={user} isSelf={loggedUser?.id === user.id} />
 	)
 }
