54b2e286fc1da5b6e0f1e70372cbfbcffd75e56c
Kazz <i94773007@gmail.com>
2025-01-25 14:40:34 +0100

    feat: enhance loading page with custom styles and animation

--- src/app/(site)/loading.tsx
+++ src/app/(site)/loading.tsx
@@ -1,9 +1,9 @@
 
 export default function LoadingPage() {
 	return <div className='h-full pb-20 grid justify-center items-center gap-4'>
-		<div className='animate-pulse flex flex-col items-center gap-4'>
+		<div className=' flex flex-col items-center gap-4'>
 			<svg
-				className="block h-[var(--uib-size)] w-[var(--uib-size)] transform-origin-center overflow-visible"
+				className="container"
 				x="0px"
 				y="0px"
 				viewBox="0 0 37 37"
@@ -12,14 +12,67 @@ export default function LoadingPage() {
 				preserveAspectRatio="xMidYMid meet"
 			>
 				<path
-					className="track stroke-[5] fill-none stroke-current opacity-[var(--uib-bg-opacity)]"
+					className="track"
+					fill="none"
+					stroke-width="5"
 					pathLength="100"
 					d="M36.63 31.746 c0 -13.394 -7.3260000000000005 -25.16 -18.13 -31.375999999999998 C7.696 6.66 0.37 18.352 0.37 31.746 c5.328 3.108 11.544 4.8839999999999995 18.13 4.8839999999999995 S31.301999999999996 34.854 36.63 31.746z"></path>
 				<path
-					className="car stroke-[5] fill-none stroke-current stroke-dasharray-[15_85] stroke-dashoffset-0 rounded-[100%]"
+					className="car"
+					fill="none"
+					stroke-width="5"
 					pathLength="100"
 					d="M36.63 31.746 c0 -13.394 -7.3260000000000005 -25.16 -18.13 -31.375999999999998 C7.696 6.66 0.37 18.352 0.37 31.746 c5.328 3.108 11.544 4.8839999999999995 18.13 4.8839999999999995 S31.301999999999996 34.854 36.63 31.746z"></path>
 			</svg>
+
+			<style>
+
+			</style>
+
+
+
+			<style>
+				{` 
+				.container {
+					--uib - size: 37px;
+					--uib-color: white;
+					--uib-speed: .9s;
+					--uib-bg-opacity: .1;
+					height: var(--uib-size);
+					width: var(--uib-size);
+					transform-origin: center;
+					overflow: visible;
+				}
+
+				.car {
+					fill: none;
+					stroke: var(--uib-color);
+					stroke-dasharray: 15, 85;
+					stroke-dashoffset: 0;
+					stroke-linecap: round;
+					animation: travel var(--uib-speed) linear infinite;
+					will-change: stroke-dasharray, stroke-dashoffset;
+					transition: stroke 0.5s ease;
+				}
+
+				.track {
+					stroke: var(--uib-color);
+					opacity: var(--uib-bg-opacity);
+					transition: stroke 0.5s ease;
+				}
+
+				@keyframes travel {
+					0% {
+						stroke-dashoffset: 0;
+					}
+
+					100% {
+						stroke-dashoffset: 100;
+					}
+				}
+				`}
+			</style>
+
 
 			<h1 className='text-3xl pl-2'>Loading...</h1>
 		</div>
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -10,6 +10,8 @@ export default async function HomePage() {
 	const session = await getServerSession(authOptions)
 	const isUserLogged = Boolean(session?.user)
 
+	await new Promise(resolve => setTimeout(resolve, 30000))
+
 	return (
 		<div className="min-h-screen">
 			<main>
