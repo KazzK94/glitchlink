d5f6c4a675737c75e3adf6fefc9db84623d58ae6
Kazz <i94773007@gmail.com>
2025-01-25 21:54:58 +0100

    added mock note for conversation page (still not available)

--- src/app/(site)/dm/[username]/page.tsx
+++ src/app/(site)/dm/[username]/page.tsx
@@ -1,13 +1,13 @@
 import { Container } from '@/components/common/Container'
 
 export default async function ConversationPage({ params }: { params: { username: string } }) {
-	
+
 	// const conversation = await getConversationByTargetUsername({ username: params.username })
 
 	return (
 		<Container asSection className='h-full'>
-			<h1 className='text-3xl'>
-				Conversation with <span className='font-semibold text-blue-300'>@{params.username}</span>
+			<h1 className='text-2xl pt-4'>
+				A conversation with <span className='font-semibold text-blue-300'>@{params.username}</span> would go here
 			</h1>
 		</Container>
 	)
