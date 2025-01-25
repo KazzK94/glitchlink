aba48b8de5dac7d30fee0837625c36527e972a93
Kazz <i94773007@gmail.com>
2025-01-25 19:16:13 +0100

    feat: redesign LoginPage and LoginForm with improved styles

--- src/app/(site)/(auth)/login/page.tsx
+++ src/app/(site)/(auth)/login/page.tsx
@@ -1,19 +1,11 @@
 
 import { LoginForm } from '@/components/auth/LoginForm'
 import { Container } from '@/components/common/Container'
-import Link from 'next/link'
-import { Suspense } from 'react'
 
 export default function LoginPage() {
 	return (
-		<Container asSection className='mt-6 max-w-96 flex flex-col gap-1'>
-			<h1 className="text-2xl mb-2 text-center">Login</h1>
-			<Suspense fallback={<div>Loading login form...</div>} >
-				<LoginForm />
-			</Suspense>
-			<Link href='/register' className="text-cyan-400 mt-4 text-center w-fit mx-auto">
-				Don&apos;t have an account yet? Register here!
-			</Link>
+		<Container asSection className='py-8 pb-6'>
+			<LoginForm />
 		</Container>
 	)
 }
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -11,6 +11,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useState } from 'react'
+import Link from 'next/link'
 
 export function LoginForm() {
 
@@ -59,35 +60,67 @@ export function LoginForm() {
 
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-96 mx-auto space-y-3">
-				<FormField
-					control={form.control}
-					name="username"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Username</FormLabel>
-							<FormControl>
-								<Input {...field} />
-							</FormControl>
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password</FormLabel>
-							<FormControl>
-								<Input {...field} type="password" />
-							</FormControl>
-						</FormItem>
-					)}
-				/>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="w-full max-w-sm mx-auto bg-gray-800/80 p-6 rounded-2xl shadow-lg border border-gray-400">
 
-				{error && <p className='bg-red-600/90 text-white rounded p-2'>{error}</p>}
+				<div className='mb-3'>
+					<h1 className="text-3xl font-bold text-center text-white">Welcome Back</h1>
+					<p className="mt-1 text-base text-center text-gray-200">Log in to continue</p>
+				</div>
 
-				<Button type="submit" disabled={isLoading} variant='secondary' className='w-full'>Log in</Button>
+				<div>
+					<FormField
+						control={form.control}
+						name="username"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="ml-0.5 text-base font-medium text-gray-200">Username</FormLabel>
+								<FormControl>
+									<Input
+										{...field}
+										className="mt-1 block w-full rounded-sm bg-black/15 border-gray-500 focus:ring-blue-500 focus:border-blue-500"
+									/>
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem className='mt-2.5'>
+								<FormLabel className="ml-0.5 text-base font-medium text-gray-200">Password</FormLabel>
+								<FormControl>
+									<Input
+										{...field}
+										type="password"
+										className="mt-1 block w-full rounded-sm bg-black/15 border-gray-500 focus:ring-blue-500 focus:border-blue-500"
+									/>
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+				</div>
+
+				{error && <p className='mt-3 bg-red-100 text-red-700 text-sm p-3 rounded-lg'>{error}</p>}
+
+				<Button
+					type="submit"
+					disabled={isLoading}
+					variant='secondary'
+					className='mt-4 px-4 py-5 w-full rounded-xl text-base font-semibold text-black bg-gradient-to-r from-cyan-100 to-purple-100 hover:from-cyan-200 hover:to-purple-200'>
+					{isLoading ? "Logging in..." : "Log in"}
+				</Button>
+
+				<p className='mt-5 mx-auto md:text-sm font-semibold text-pretty flex flex-col md:flex-row gap-x-1.5 justify-center items-center'>
+					<span>
+						You don&apos;t have an account yet?
+					</span>
+					<Link href='/register' className="text-cyan-400 hover:text-cyan-500">
+						Register here!
+					</Link>
+				</p>
 			</form>
 		</Form>
 	)
