e1ef342bdd4fa5dcdfe1d99a15c24245f4876d7d
Kazz <i94773007@gmail.com>
2025-01-29 22:47:03 +0100

    fix: GameCard: slight change to bg to show while loading images

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -3,7 +3,7 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { AddGameToCollectionButton } from './AddGameToCollectionButton'
 
-interface GameCardProps {
+interface VideoGameCardProps {
 	externalId: number
 	title: string
 	imageUrl: string
@@ -11,7 +11,7 @@ interface GameCardProps {
 	userIsLogged: boolean
 }
 
-export function GameCard({ externalId, title, imageUrl, isOwned = false, userIsLogged }: GameCardProps) {
+export function VideoGameCard({ externalId, title, imageUrl, isOwned = false, userIsLogged }: VideoGameCardProps) {
 
 	const croppedImageUrl = imageUrl.replace('media/', 'media/crop/600/400/')
 
@@ -22,7 +22,7 @@ export function GameCard({ externalId, title, imageUrl, isOwned = false, userIsL
 			<img
 				src={croppedImageUrl}
 				alt={title}
-				className="transition h-full min-w-full object-cover duration-300 ease-in-out group-hover:opacity-95 group-hover:saturate-150 bg-black"
+				className="transition h-full min-w-full object-cover duration-300 ease-in-out group-hover:opacity-95 group-hover:saturate-150 bg-gradient-to-br from-black via-slate-700 to-black"
 			/>
 			<CardContent className="absolute inset-0 flex flex-col justify-end p-4 bg-gradient-to-t from-black to-transparent">
 				{/* TODO: Make it be maximum 2 lines (add ellipsis on overflow instead of 3rd line) */}
