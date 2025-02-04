332214541addda60b1c53bf2c2f51bd4dba4a674
Kazz <i94773007@gmail.com>
2025-02-04 15:13:45 +0100

    feat: add user session check and redirect to / in login and registration pages

--- src/app/(site)/(auth)/login/page.tsx
+++ src/app/(site)/(auth)/login/page.tsx
@@ -2,7 +2,15 @@
 import { LoginForm } from '@/components/auth/LoginForm'
 import { Container } from '@/components/common/Container'
 
-export default function LoginPage() {
+import { getUserFromSession } from '@/services/auth'
+import { redirect } from 'next/navigation'
+
+export default async function LoginPage() {
+	const loggedUser = await getUserFromSession()
+	if(loggedUser) {
+		redirect('/')
+	}
+
 	return (
 		<Container asSection className='py-8 pb-6'>
 			<LoginForm />
--- src/app/(site)/(auth)/register/page.tsx
+++ src/app/(site)/(auth)/register/page.tsx
@@ -1,7 +1,15 @@
+
 import { Container } from '@/components/common/Container'
 import { RegisterForm } from '@/components/auth/RegisterForm'
 
-export default function RegisterPage() {
+import { getUserFromSession } from '@/services/auth'
+import { redirect } from 'next/navigation'
+
+export default async function RegisterPage() {
+	const loggedUser = await getUserFromSession()
+	if(loggedUser) {
+		redirect('/')
+	}
 
 	return (
 		<Container asSection className='py-8 pb-6'>
