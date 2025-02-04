ba2bd844952fb4772dac912f7c821c8339a727ba
Kazz <i94773007@gmail.com>
2025-02-04 12:54:12 +0100

    fix: fixed avatar size in profile edit page

--- src/app/(site)/profile/edit/page.tsx
+++ src/app/(site)/profile/edit/page.tsx
@@ -25,7 +25,9 @@ export default async function EditProfilePage() {
 					<form className='bg-slate-800 px-4 py-3 rounded-lg'>
 						<h2 className='text-xl border-b border-gray-400 pl-1 pb-0.5 mb-4'>Profile Picture</h2>
 						<div className='flex flex-col items-center gap-4'>
-							<Avatar src={user?.avatar} className='max-w-[min(10rem,80%)]' editable />
+							<div className='max-w-[min(10rem,85%)] p-2'>
+								<Avatar src={user?.avatar} editable />
+							</div>
 							<p className='italic text-sm opacity-80 text-center'>Click on the picture to change it!</p>
 						</div>
 					</form>
