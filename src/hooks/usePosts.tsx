443c99800257a4816f013fdb6f5a0b5fa68f788b
Kazz <i94773007@gmail.com>
2025-02-24 00:46:52 +0100

    refactor PostParsedContent component; move to new directory and enhance content parsing to include URLs

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -2,27 +2,38 @@
 
 import { PostHashtag } from '@/components/posts/parseds/Hashtag'
 import { PostMention } from '@/components/posts/parseds/Mention'
+import Link from 'next/link'
 import { Fragment } from 'react'
 
 export function usePosts() {
 	const parsePostContent = (text: string) => {
 		const words = text.split(/(\s+)/)
 		return words.map((word, index) => {
-		  // Match mentions: start with @, followed by letter, then letters/numbers/underscores, followed by any characters
-		  const mentionMatch = word.match(/^@([a-zA-Z]\w+)(.*)$/)
-		  if (mentionMatch) {
-			return (
-			  <Fragment key={index}>
-				<PostMention username={mentionMatch[1]} />
-				{mentionMatch[2]}
-			  </Fragment>
-			)
-		  } else if (/^#\w+$/.test(word)) {
-			return <PostHashtag key={index} hashtag={word} />
-		  }
-		  return word
+			// Match mentions: start with @, followed by letter, then letters/numbers/underscores, followed by any characters
+			const mentionMatch = word.match(/^@([a-zA-Z]\w+)(.*)$/)
+			if (mentionMatch) {
+				return (
+					<Fragment key={index}>
+						<PostMention username={mentionMatch[1]} />
+						{mentionMatch[2]}
+					</Fragment>
+				)
+			}
+			const hashtagMatch = word.match(/^#(\w+)(.*)$/)
+			if (hashtagMatch) {
+				return <PostHashtag key={index} hashtag={word} />
+			}
+			const urlMatch = word.match(/(https?:\/\/[^\s]+)/)
+			if (urlMatch) {
+				return (
+					<Link key={index} href={urlMatch[1]} className='text-blue-500 italic hover:underline'>
+						{urlMatch[1]}
+					</Link>
+				)
+			}
+			return word
 		})
-	  }
+	}
 
 	return { parsePostContent }
 }
